155feaba207c3bde4a465720de6b64a921fabb03
eyder-suarez <ey_der19@hotmail.com>
2023-11-13 15:51:59 -0500

    Se agrega contenido al componente about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./shared/menu/menu.component";
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, MenuComponent]
+    imports: [CommonModule, RouterOutlet, MenuComponent, InicioComponent]
 })
 export class AppComponent {
   title = 'website';
