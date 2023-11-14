68d3ce6f8c6dd401cec3028713f02635d46c9567
Eyder Suarez <ey_der19@hotmail.com>
2023-11-14 15:59:32 -0500

    Se hacen ajustes en las rutas del menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ProyectsComponent } from './pages/proyects/proyects.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 export const routes: Routes = [
 
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent},
   { path: 'servicios', component: ServiciosComponent },
   { path: 'proyectos', component: ProyectsComponent },
-  { path: 'contacto', component: ContactComponent }
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', component: InicioComponent }
 
 ];
